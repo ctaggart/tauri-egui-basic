9103a23904299858cca9745e87c14b86a8ec59f6
Cameron Taggart <cameron.taggart@gmail.com>
2022-10-11 15:43:57 -0400

    fix build https://github.com/tauri-apps/tauri-egui/issues/12

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,39 @@
 
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use tauri::{RunEvent, State};
-use tracing::info;
+use tauri::{RunEvent, Manager};
+use tracing::{debug, info};
 
 fn main() {
     tracing_subscriber::fmt::init();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![open_native_window])
         .setup(|app| {
             app.wry_plugin(tauri_egui::EguiPluginBuilder::new(app.handle()));
+
+            let native_options = eframe::NativeOptions {
+                drag_and_drop_support: true,
+                initial_window_size: Some([1280.0, 1024.0].into()),
+                ..Default::default()
+            };
+
+            app.state::<tauri_egui::EguiPluginHandle>()
+            .create_window(
+                "main".to_string(),
+                Box::new(|_cc| Box::new(BasicApp::default())),
+                "Basic App".into(),
+                native_options,
+            )
+            .unwrap();
+
             Ok(())
         })
         .build(tauri::generate_context!())
         .expect("error while running tauri application")
         .run(|_app, event| {
             if let RunEvent::WindowEvent { label, event, .. } = event {
-                println!("{} {:?}", label, event);
+                debug!("{} {:?}", label, event);
             }
         });
-}
-
-#[tauri::command]
-async fn open_native_window(
-    egui_handle: State<'_, tauri_egui::EguiPluginHandle>,
-) -> Result<(), ()> {
-    let native_options = eframe::NativeOptions {
-        drag_and_drop_support: true,
-        initial_window_size: Some([1280.0, 1024.0].into()),
-        ..Default::default()
-    };
-
-    let _window = egui_handle
-        .create_window(
-            "native-window".to_string(),
-            Box::new(|cc| Box::new(BasicApp::default())),
-            "Login".into(),
-            native_options,
-        )
-        .unwrap();
-
-    Ok(())
 }
 
 #[derive(Default)]
